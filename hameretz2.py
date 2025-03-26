52a1ddf85e51dd4d41f3c0692eb9c7fa34417edc
OmerS16 <omerseter@gmail.com>
2025-03-26 17:36:31 +0200

    Updated code

--- hameretz2.py
+++ hameretz2.py
@@ -9,59 +9,61 @@ from datetime import datetime
 import time
 import re
 
-heb_to_eng = {
-    "בינו׳": "Jan",
-    "בפבר׳":"Feb",
-    "במרץ":"Mar",
-    "באפר׳":"Apr",
-    "במאי":"May",
-    "ביונ׳":"Jun",
-    "ביול׳":"Jul",
-    "באוג׳":"Aug",
-    "בספט׳":"Sep",
-    "באוק׳":"Oct",
-    "בנוב׳":"Nov",
-    "בדצמ׳":"Dec"
-    }
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--window-size=1920,1080")
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-
-service = Service('chromedriver.exe')
-driver = webdriver.Chrome(service=service, options=chrome_options)
-
-driver.get("https://www.hameretz2.org/")
-
-wait = WebDriverWait(driver, 10)
-events_section = wait.until(EC.presence_of_element_located((By.ID, "comp-m4r7k1ni")))
-
-events = []
-
-for event in events_section.find_elements(By.CLASS_NAME, 'qElViY'):
-    title = event.find_element(By.CLASS_NAME, 'DjQEyU').get_attribute('textContent')
-    if any(word in title for word in ['קולנוע', 'לנדסקייפ']):
-        continue
-    wait = WebDriverWait(event, 10)
-    button = event.find_element(By.CSS_SELECTOR, ".sr_1jEj.sFpk6n3.sUMQ4T0.oDFuHIw---priority-4-link.s__6KuQ2f")
-    time.sleep(3)
-    button.click()
-    date_elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Ke8eTf.tnxjtm")))
-    date = date_elem.get_attribute('textContent').strip()
-    for heb, eng in heb_to_eng.items():
-        date = date.replace(heb, eng)
-    date, hour = date.split(', ')
-    hour = hour[:5]
-    date = date + ' ' + str(hour)
-    date = datetime.strptime(date, '%d %b %Y %H:%M')
-    link = event.find_element(By.CSS_SELECTOR, '.DjQEyU.m022zm.aUkG34').get_attribute('href')
-    img = event.find_element(By.CSS_SELECTOR, '.L5u5gG.sGc29C2')
-    img = img.find_element(By.TAG_NAME, 'img').get_attribute('src')
-    img = img.replace('blur_2,', '')
-    img = re.sub(r"w_\d+", "w_500", img)
-    img = re.sub(r"h_\d+", "h_300", img)
-    events.append({'show_name':title, 'date':date, 'link':link, 'img':img, 'venue':'המרץ 2'})
+def hameretz2():
+    heb_to_eng = {
+        "בינו׳": "Jan",
+        "בפבר׳":"Feb",
+        "במרץ":"Mar",
+        "באפר׳":"Apr",
+        "במאי":"May",
+        "ביונ׳":"Jun",
+        "ביול׳":"Jul",
+        "באוג׳":"Aug",
+        "בספט׳":"Sep",
+        "באוק׳":"Oct",
+        "בנוב׳":"Nov",
+        "בדצמ׳":"Dec"
+        }
     
-events = pd.DataFrame(events)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    
+    service = Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    
+    driver.get("https://www.hameretz2.org/")
+    
+    wait = WebDriverWait(driver, 10)
+    events_section = wait.until(EC.presence_of_element_located((By.ID, "comp-m4r7k1ni")))
+    
+    events = []
+    
+    for event in events_section.find_elements(By.CLASS_NAME, 'qElViY'):
+        title = event.find_element(By.CLASS_NAME, 'DjQEyU').get_attribute('textContent')
+        if any(word in title for word in ['קולנוע', 'לנדסקייפ']):
+            continue
+        wait = WebDriverWait(event, 10)
+        button = event.find_element(By.CSS_SELECTOR, ".sr_1jEj.sFpk6n3.sUMQ4T0.oDFuHIw---priority-4-link.s__6KuQ2f")
+        time.sleep(3)
+        button.click()
+        date_elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".Ke8eTf.tnxjtm")))
+        date = date_elem.get_attribute('textContent').strip()
+        for heb, eng in heb_to_eng.items():
+            date = date.replace(heb, eng)
+        date, hour = date.split(', ', 1)
+        hour = hour[:5]
+        date = date + ' ' + str(hour)
+        date = datetime.strptime(date, '%d %b %Y %H:%M')
+        link = event.find_element(By.CSS_SELECTOR, '.DjQEyU.m022zm.aUkG34').get_attribute('href')
+        img = event.find_element(By.CSS_SELECTOR, '.L5u5gG.sGc29C2')
+        img = img.find_element(By.TAG_NAME, 'img').get_attribute('src')
+        img = img.replace('blur_2,', '')
+        img = re.sub(r"w_\d+", "w_500", img)
+        img = re.sub(r"h_\d+", "h_300", img)
+        events.append({'show_name':title, 'date':date, 'link':link, 'img':img, 'venue':'המרץ 2'})
+        
+    events = pd.DataFrame(events)
+    return events
